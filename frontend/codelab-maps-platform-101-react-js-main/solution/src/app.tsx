689b37636d771873618b1ec9df75b9c48ef11776
turtlelazy <ishraqmahid@gmail.com>
2024-11-02 15:25:48 -0400

    icon

--- frontend/codelab-maps-platform-101-react-js-main/solution/src/app.tsx
+++ frontend/codelab-maps-platform-101-react-js-main/solution/src/app.tsx
@@ -1,41 +1,20 @@
-/**
- * Copyright 2024 Google LLC
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *    https://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
-*/
-
-import React, {useEffect, useState, useRef, useCallback} from 'react';
-import {createRoot} from 'react-dom/client';
-
-import {
-  APIProvider,
-  Map,
-  useMap,
-  AdvancedMarker,
-  MapCameraChangedEvent,
-  Pin
-} from '@vis.gl/react-google-maps';
-
-import {MarkerClusterer} from '@googlemaps/markerclusterer';
-import type {Marker} from '@googlemaps/markerclusterer';
+import React, { useEffect, useState, useRef, useCallback } from 'react';
+import { createRoot } from 'react-dom/client';
+import { APIProvider, Map, useMap, AdvancedMarker, MapCameraChangedEvent, Pin } from '@vis.gl/react-google-maps';
+import { MarkerClusterer } from '@googlemaps/markerclusterer';
+import type { Marker } from '@googlemaps/markerclusterer';
 import google_maps_api_key from '../secrets';
-import {Circle} from './components/circle'
-console.log('google_maps_api_key:', google_maps_api_key);
-type Poi ={ key: string, location: google.maps.LatLngLiteral, name: string };
+import { Circle } from './components/circle';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGavel, faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
+import { InfoWindow } from '@vis.gl/react-google-maps';
+
+type Poi = { key: string, location: google.maps.LatLngLiteral, name: string };
 const locations: Poi[] = [
-  {key: 'operaHouse', location: { lat: -33.8567844, lng: 151.213108  }, name: 'Sydney Opera House'},
-  {key: 'tarongaZoo', location: { lat: -33.8472767, lng: 151.2188164 }, name: 'Taronga Zoo'},
-  {key: 'manlyBeach', location: { lat: -33.8209738, lng: 151.2563253 }, name: 'Manly Beach'},
+  { key: 'operaHouse', location: { lat: -33.8567844, lng: 151.213108 }, name: 'Sydney Opera House' },
+  { key: 'tarongaZoo', location: { lat: -33.8472767, lng: 151.2188164 }, name: 'Taronga Zoo' },
+  { key: 'manlyBeach', location: { lat: -33.8209738, lng: 151.2563253 }, name: 'Manly Beach' },
 ];
 
 const App = () => (
@@ -43,17 +22,15 @@ const App = () => (
     <Map
       defaultZoom={13}
       defaultCenter={{ lat: -33.860664, lng: 151.208138 }}
-      onCameraChanged={ (ev: MapCameraChangedEvent) =>
+      onCameraChanged={(ev: MapCameraChangedEvent) =>
         console.log('camera changed:', ev.detail.center, 'zoom:', ev.detail.zoom)
       }
       mapId='da37f3254c6a6d1c'
-      >
-    <PoiMarkers pois={locations} />
+    >
+      <PoiMarkers pois={locations} />
     </Map>
   </APIProvider>
 );
-
-import { InfoWindow } from '@vis.gl/react-google-maps';
 
 const PoiMarkers = (props: { pois: Poi[] }) => {
   const map = useMap();
@@ -109,6 +86,7 @@ const PoiMarkers = (props: { pois: Poi[] }) => {
         fillOpacity={0.3}
       />
       {props.pois.map((poi: Poi) => (
+        // MARKER COMPONENT
         <AdvancedMarker
           key={poi.key}
           position={poi.location}
@@ -116,7 +94,9 @@ const PoiMarkers = (props: { pois: Poi[] }) => {
           clickable={true}
           onClick={() => handleClick(poi)}
         >
-          <Pin background={'#FBBC04'} glyphColor={'#000'} borderColor={'#000'} />
+          <Pin background={'#FBBC04'} glyphColor={'#000'} borderColor={'#000'}>
+            <FontAwesomeIcon icon={faGavel} color='brown' />
+          </Pin>
         </AdvancedMarker>
       ))}
       {selectedPoi && (
@@ -134,7 +114,4 @@ const PoiMarkers = (props: { pois: Poi[] }) => {
 export default App;
 
 const root = createRoot(document.getElementById('app'));
-root.render(
-      <App />
-  );
-
+root.render(<App />);
