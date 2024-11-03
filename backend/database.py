a3ff4c6b2ae9928bc74cc2e85ce84339efa9c9cd
SiennaChien <sienna.chien@gmail.com>
2024-11-03 03:18:41 -0500

    POST for user input done, GET not yet

--- backend/database.py
+++ backend/database.py
@@ -9,25 +9,26 @@ def create_table():
     conn = create_connection()
     cursor = conn.cursor()
 
+
     #user input data
     cursor.execute('''
-        CREATE TABLE IF NOT EXISTS services (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            name TEXT NOT NULL,
-            servicetype TEXT,
-            extrafilters TEXT,
-            demographic TEXT,
-            website TEXT,
-            summary TEXT,
-            address TEXT,
-            coordinates TEXT,
-            neighborhoods TEXT,
-            hours TEXT,
-            phone TEXT,
-            languages TEXT,
-            googlelink TEXT,
-            source TEXT
-        )
+    CREATE TABLE IF NOT EXISTS services (
+        ID TEXT PRIMARY KEY,
+        name TEXT NOT NULL,
+        servicetype TEXT,
+        extrafilters TEXT,
+        demographic TEXT,
+        website TEXT,
+        summary TEXT,
+        address TEXT,
+        coordinates TEXT,
+        neighborhoods TEXT,
+        hours TEXT,
+        phone TEXT,
+        languages TEXT,
+        googlelink TEXT,
+        source TEXT
+    )
     ''')
 
 
@@ -36,12 +37,9 @@ def create_table():
         CREATE TABLE IF NOT EXISTS reviews (
             service_id TEXT,
             reviews TEXT
+            update INTEGER
         )
     ''')
 
     conn.commit()
     conn.close()
-
-# Ensure the table is created
-create_table()
-
